9e1ddf9696c1ed01499ccafa89073aeca58f9e78
lununes <luciana.mn@gmail.com>
2026-01-08 14:01:49 -0300

    Estrutura das páginas do curso - index e Mod 1

---
+++ assets/js/course.js
@@ -0,0 +1,196 @@
+const course = {
+  title: 'Curso | Formação Continuada em Toxicologia Aplicada a Metais',
+  modules: [
+    {
+      title: "Módulo 1: Bases conceituais e políticas da Vigilância em Saúde",
+      items: [
+        // { type: "link", icon: "welcome", title: "Boas-vindas ao módulo", path: "/modulo1/apresentacao.html" },
+        {
+          type: "accordion",
+          title: "Aula 1 | Construção histórica conceitos, componentes, ações e organização da Vigilância em Saúde",
+          items: [
+            { type: "link", icon: "lesson", title: "Sobre esta aula", path: "/modulo1/aula1/sobre-aula.html" },
+            { type: "link", icon: "lesson", title: "Tópico 1: Introdução", path: "/modulo1/aula1/topico1.html" },
+            { type: "link", icon: "lesson", title: "Tópico 2: Origens da vigilância em saúde no mundo", path: "/modulo1/aula1/topico2.html" },
+            { type: "link", icon: "lesson", title: "Tópico 3: A vigilância em saúde no Brasil: trajetória histórica", path: "/modulo1/aula1/topico3.html" },
+            { type: "link", icon: "lesson", title: "Tópico 4: Componentes e ações de vigilância em saúde", path: "/modulo1/aula1/topico4.html" },
+            { type: "link", icon: "lesson", title: "Questões de fixação", path: "/modulo1/aula1/atividade.html" }
+          ]
+        },
+        {
+          type: "accordion",
+          title: "Aula 2 | Política Nacional de Vigilância em Saúde e Planejamento",
+          items: [
+            { type: "link", icon: "lesson", title: "Sobre esta aula", path: "/modulo1/aula2/sobre-aula.html" },
+            { type: "link", icon: "lesson", title: "Tópico 1: Bases conceituais de políticas da vigilância em saúde e planejamento", path: "/modulo1/aula2/topico1.html" },
+            { type: "link", icon: "lesson", title: "Tópico 2: Planejamento em saúde e seus modelos", path: "/modulo1/aula2/topico2.html" },
+            { type: "link", icon: "lesson", title: "Tópico 3: Experiências e práticas", path: "/modulo1/aula2/topico3.html" },
+            { type: "link", icon: "lesson", title: "Questões de fixação", path: "/modulo1/aula2/atividade.html" }
+          ]
+        },
+        {
+          type: "accordion",
+          title: "Aula 3 | Sistema Nacional de Vigilância em Saúde",
+          items: [
+            { type: "link", icon: "lesson", title: "Sobre esta aula", path: "/modulo1/aula3/sobre-aula.html" },
+            { type: "link", icon: "lesson", title: "Tópico 1: A vigilância em saúde", path: "/modulo1/aula3/topico1.html" },
+            { type: "link", icon: "lesson", title: "Tópico 2: Componentes da vigilância em saúde", path: "/modulo1/aula3/topico2.html" },
+            { type: "link", icon: "lesson", title: "Tópico 3: Etapas do processo de vigilância em saúde", path: "/modulo1/aula3/topico3.html" },
+            { type: "link", icon: "lesson", title: "Tópico 4: Conceitos e ferramentas importantes para a vigilância em saúde", path: "/modulo1/aula3/topico4.html" },
+            { type: "link", icon: "lesson", title: "Questões de fixação", path: "/modulo1/aula3/atividade.html" }
+          ]
+        },
+        {
+          type: "accordion",
+          title: "Aula 4 | Emergências em Saúde Pública",
+          items: [
+            { type: "link", icon: "lesson", title: "Sobre esta aula", path: "/modulo1/aula4/sobre-aula.html" },
+            { type: "link", icon: "lesson", title: "Tópico 1: Contextualização, Conceitos e Linha do Tempo", path: "/modulo1/aula4/topico1.html" },
+            { type: "link", icon: "lesson", title: "Tópico 2: Governança Global e o Ciclo de Gestão de Emergência", path: "/modulo1/aula4/topico2.html" },
+            { type: "link", icon: "lesson", title: "Tópico 3: Estudo de Caso", path: "/modulo1/aula4/topico3.html" },
+            { type: "link", icon: "lesson", title: "Questões de fixação", path: "/modulo1/aula4/atividade.html" }
+          ]
+        },
+        // { type: "link", icon: "closing", title: "Encerramento do módulo", path: "/modulo1/encerramento.html" }
+      ]
+    },
+
+    {
+      title: "Módulo 2: Conceitos básicos da Toxicologia: caracterização de exposição e qualificação da ação",
+      items: [
+        // { type: "link", icon: "welcome", title: "Boas-vindas ao módulo", path: "/modulo2/apresentacao.html" },
+        {
+          type: "accordion",
+          title: "Aula 1 | Introdução à Toxicologia e Contaminantes Químicos",
+          items: [
+            { type: "link", icon: "lesson", title: "Sobre esta aula", path: "/modulo2/aula1/sobre-aula.html" },
+            { type: "link", icon: "lesson", title: "Tópico 1: Fundamentos da Toxicologia e Contaminantes", path: "/modulo2/aula1/topico1.html" },
+            { type: "link", icon: "lesson", title: "Tópico 2: Casos e Relatos de Sucesso/Fracasso", path: "/modulo2/aula1/topico2.html" },
+            { type: "link", icon: "lesson", title: "Questões de fixação", path: "/modulo2/aula1/atividade.html" }
+          ]
+        },
+        {
+          type: "accordion",
+          title: "Aula 2 | Fundamentos da Toxicocinética, Toxicodinâmica da Exposição Tóxica e Epidemiologia",
+          items: [
+            { type: "link", icon: "lesson", title: "Sobre esta aula", path: "/modulo2/aula2/sobre-aula.html" },
+            { type: "link", icon: "lesson", title: "Tópico 1: Introdução", path: "/modulo2/aula2/topico1.html" },
+            { type: "link", icon: "lesson", title: "Tópico 2: Conceitos Fundamentais", path: "/modulo2/aula2/topico2.html" },
+            { type: "link", icon: "lesson", title: "Tópico 3: Toxicologia e epidemiologia na avaliação de riscos e efeitos à saúde", path: "/modulo2/aula2/topico3.html" },
+            { type: "link", icon: "lesson", title: "Tópico 4: Estudo de Casos: Avaliação dos efeitos da exposição a metais tóxicos", path: "/modulo2/aula2/topico4.html" },
+            { type: "link", icon: "lesson", title: "Questões de fixação", path: "/modulo2/aula2/atividade.html" }
+          ]
+        },
+        {
+          type: "accordion",
+          title: "Aula 3 | Toxicologia dos medicamentos e tabagismo",
+          items: [
+            { type: "link", icon: "lesson", title: "Sobre esta aula", path: "/modulo2/aula3/sobre-aula.html" },
+            { type: "link", icon: "lesson", title: "Tópico 1: Automedicação, o uso indevido de medicamentos e intoxicação", path: "/modulo2/aula3/topico1.html" },
+            { type: "link", icon: "lesson", title: "Tópico 2: Fatores que influenciam a toxicidade dos medicamentos", path: "/modulo2/aula3/topico2.html" },
+            { type: "link", icon: "lesson", title: "Tópico 3: Classificação de medicamentos", path: "/modulo2/aula3/topico3.html" },
+            { type: "link", icon: "lesson", title: "Tópico 4: Tabagismo", path: "/modulo2/aula3/topico4.html" },
+            { type: "link", icon: "lesson", title: "Tópico 5: Composição química do tabaco e da fumaça do cigarro", path: "/modulo2/aula3/topico5.html" },
+            { type: "link", icon: "lesson", title: "Tópico 6: Toxicocinética da nicotina", path: "/modulo2/aula3/topico6.html" },
+            { type: "link", icon: "lesson", title: "Tópico 7: Efeitos tóxicos do tabaco", path: "/modulo2/aula3/topico7.html" },
+            { type: "link", icon: "lesson", title: "Questões de fixação", path: "/modulo2/aula3/atividade.html" }
+          ]
+        },
+        {
+          type: "accordion",
+          title: "Aula 4 | Toxicologia dos alimentos e dos agrotóxicos",
+          items: [
+            { type: "link", icon: "lesson", title: "Sobre esta aula", path: "/modulo2/aula4/sobre-aula.html" },
+            { type: "link", icon: "lesson", title: "Tópico 1: O que é toxicologia dos alimentos?", path: "/modulo2/aula4/topico1.html" },
+            { type: "link", icon: "lesson", title: "Tópico 2: Classificação dos contaminantes alimentares", path: "/modulo2/aula4/topico2.html" },
+            { type: "link", icon: "lesson", title: "Questões de fixação", path: "/modulo2/aula4/atividade.html" }
+          ]
+        },
+        {
+          type: "accordion",
+          title: "Aula 5 | Toxicologia Ocupacional",
+          items: [
+            { type: "link", icon: "lesson", title: "Sobre estas aula", path: "/modulo2/aula5/sobre-aula.html" },
+            { type: "link", icon: "lesson", title: "Tópico 1: Toxicologia e ambientes de trabalho", path: "/modulo2/aula5/topico1.html" },
+            { type: "link", icon: "lesson", title: "Tópico 2: Monitoramento da exposição ocupacional", path: "/modulo2/aula5/topico2.html" },
+            { type: "link", icon: "lesson", title: "Tópico 3: Monitoramento ambiental (MA)", path: "/modulo2/aula5/topico3.html" },
+            { type: "link", icon: "lesson", title: "Tópico 4: Monitoramento biológico (MB) ou Biomonitoramento", path: "/modulo2/aula5/topico4.html" },
+            { type: "link", icon: "lesson", title: "Tópico 5: Fase clínica: vigilância da saúde e diagnóstico clínico", path: "/modulo2/aula5/topico5.html" },
+            { type: "link", icon: "lesson", title: "Questões de fixação", path: "/modulo2/aula5/atividade.html" }
+          ]
+        },
+        // { type: "link", icon: "activity", title: "Atividade", path: "/modulo2/atividade.html" },
+        // { type: "link", icon: "closing", title: "Encerramento do módulo", path: "/modulo2/encerramento.html" }
+      ]
+    },
+
+    {
+      title: "Módulo 3: Toxicologia Clínica aplicada aos Metais",
+      items: [
+        // { type: "link", icon: "welcome", title: "Boas-vindas ao módulo", path: "/modulo3/apresentacao.html" },
+        {
+          type: "accordion",
+          title: "Aula 1 | Toxicologia aplicados aos metais",
+          items: [
+            { type: "link", icon: "lesson", title: "Sobre esta aula", path: "/modulo3/aula1/sobre-aula.html" },
+            { type: "link", icon: "lesson", title: "Tópico 1: Conceitos fundamentais e interações", path: "/modulo3/aula1/topico1.html" },
+            { type: "link", icon: "lesson", title: "Tópico 2: Casos clínicos e experiências práticas", path: "/modulo3/aula1/topico2.html" },
+            { type: "link", icon: "lesson", title: "Questões de fixação", path: "/modulo3/aula1/atividade.html" } 
+          ]
+        },
+        {
+          type: "accordion",
+          title: "Aula 2 | Abordagem inicial ao paciente potencialmente intoxicado e exames físicos para identificação de sinais e sintomas clínicos",
+          items: [
+            { type: "link", icon: "lesson", title: "Sobre esta aula", path: "/modulo3/aula2/sobre-aula.html" },
+            { type: "link", icon: "lesson", title: "Tópico 1: Aguardando SB", path: "/modulo3/aula2/topico1.html" },
+            { type: "link", icon: "lesson", title: "Tópico 2: Aguardando SB", path: "/modulo3/aula2/topico2.html" },
+            { type: "link", icon: "lesson", title: "Questões de fixação", path: "/modulo3/aula2/atividade.html" } 
+          ]
+        },
+        {
+          type: "accordion",
+          title: "Aula 3 | Interpretação de testes laboratoriais e de imagem para avaliação de intoxicação",
+          items: [
+            { type: "link", icon: "lesson", title: "Sobre esta aula", path: "/modulo3/aula3/sobre-aula.html" },
+            { type: "link", icon: "lesson", title: "Tópico 1: Aguardando SB", path: "/modulo3/aula3/topico1.html" },
+            { type: "link", icon: "lesson", title: "Tópico 2: Aguardando SB", path: "/modulo3/aula3/topico2.html" },
+            { type: "link", icon: "lesson", title: "Questões de fixação", path: "/modulo3/aula3/atividade.html" } 
+          ]
+        },
+        {
+          type: "accordion",
+          title: "Aula 4 | Vigilância epidemiológica e gestão da informação",
+          items: [
+            { type: "link", icon: "lesson", title: "Sobre esta aula", path: "/modulo3/aula4/sobre-aula.html" },
+            { type: "link", icon: "lesson", title: "Tópico 1: Aguardando SB", path: "/modulo3/aula4/topico1.html" },
+            { type: "link", icon: "lesson", title: "Tópico 2: Aguardando SB", path: "/modulo3/aula4/topico2.html" },
+            { type: "link", icon: "lesson", title: "Questões de fixação", path: "/modulo3/aula4/atividade.html" }
+          ]
+        },
+        {
+          type: "accordion",
+          title: "Aula 5 | Exposição por metais e os determinantes sociais de saúde",
+          items: [
+            { type: "link", icon: "lesson", title: "Sobre esta aula", path: "/modulo3/aula5/sobre-aula.html" },
+            { type: "link", icon: "lesson", title: "Tópico 1: Aguardando SB", path: "/modulo3/aula5/topico1.html" },
+            { type: "link", icon: "lesson", title: "Tópico 2: Aguardando SB", path: "/modulo3/aula5/topico2.html" },
+            { type: "link", icon: "lesson", title: "Questões de fixação", path: "/modulo3/aula5/atividade.html" }
+          ]
+        },
+        {
+          type: "accordion",
+          title: "Aula 6 | Comunicação de risco à saúde",
+          items: [
+            { type: "link", icon: "lesson", title: "Sobre esta aula", path: "/modulo3/aula6/sobre-aula.html" },
+            { type: "link", icon: "lesson", title: "Tópico 1: Aguardando SB", path: "/modulo3/aula6/topico1.html" },
+            { type: "link", icon: "lesson", title: "Tópico 2: Aguardando SB", path: "/modulo3/aula6/topico2.html" },
+            { type: "link", icon: "lesson", title: "Questões de fixação", path: "/modulo3/aula6/atividade.html" }
+          ]
+        },
+        // { type: "link", icon: "activity", title: "Atividade", path: "/modulo3/atividade.html" },
+        // { type: "link", icon: "closing", title: "Encerramento do módulo", path: "/modulo3/encerramento.html" }
+      ]
+    }
+  ]
+};
