425e7e56ebbcdae09a38d9806ed9af8d4851c8ec
lununes <luciana.mn@gmail.com>
2026-01-27 17:24:22 -0300

    Ajustes Aulas 1.1 e 1.2

--- assets/js/course.js
+++ assets/js/course.js
@@ -14,7 +14,7 @@ const course = {
             { type: "link", icon: "lesson", title: "Tópico 2: Origens da vigilância em saúde no mundo", path: "/modulo1/aula1/topico2.html" },
             { type: "link", icon: "lesson", title: "Tópico 3: A vigilância em saúde no Brasil: trajetória histórica", path: "/modulo1/aula1/topico3.html" },
             { type: "link", icon: "lesson", title: "Tópico 4: Componentes e ações de vigilância em saúde", path: "/modulo1/aula1/topico4.html" },
-            { type: "link", icon: "lesson", title: "Questões de fixação", path: "/modulo1/aula1/atividade.html" }
+            { type: "link", icon: "lesson", title: "Questão de Fixação", path: "/modulo1/aula1/atividade.html" }
           ]
         },
         {
@@ -25,7 +25,7 @@ const course = {
             { type: "link", icon: "lesson", title: "Tópico 1: Bases conceituais de políticas da vigilância em saúde e planejamento", path: "/modulo1/aula2/topico1.html" },
             { type: "link", icon: "lesson", title: "Tópico 2: Planejamento em saúde e seus modelos", path: "/modulo1/aula2/topico2.html" },
             { type: "link", icon: "lesson", title: "Tópico 3: Experiências e práticas", path: "/modulo1/aula2/topico3.html" },
-            { type: "link", icon: "lesson", title: "Questões de fixação", path: "/modulo1/aula2/atividade.html" }
+            { type: "link", icon: "lesson", title: "Questão de Fixação", path: "/modulo1/aula2/atividade.html" }
           ]
         },
         {
@@ -37,7 +37,7 @@ const course = {
             { type: "link", icon: "lesson", title: "Tópico 2: Componentes da vigilância em saúde", path: "/modulo1/aula3/topico2.html" },
             { type: "link", icon: "lesson", title: "Tópico 3: Etapas do processo de vigilância em saúde", path: "/modulo1/aula3/topico3.html" },
             { type: "link", icon: "lesson", title: "Tópico 4: Conceitos e ferramentas importantes para a vigilância em saúde", path: "/modulo1/aula3/topico4.html" },
-            { type: "link", icon: "lesson", title: "Questões de fixação", path: "/modulo1/aula3/atividade.html" }
+            { type: "link", icon: "lesson", title: "Questão de Fixação", path: "/modulo1/aula3/atividade.html" }
           ]
         },
         {
@@ -48,7 +48,7 @@ const course = {
             { type: "link", icon: "lesson", title: "Tópico 1: Contextualização, Conceitos e Linha do Tempo", path: "/modulo1/aula4/topico1.html" },
             { type: "link", icon: "lesson", title: "Tópico 2: Governança Global e o Ciclo de Gestão de Emergência", path: "/modulo1/aula4/topico2.html" },
             { type: "link", icon: "lesson", title: "Tópico 3: Estudo de Caso", path: "/modulo1/aula4/topico3.html" },
-            { type: "link", icon: "lesson", title: "Questões de fixação", path: "/modulo1/aula4/atividade.html" }
+            { type: "link", icon: "lesson", title: "Questão de Fixação", path: "/modulo1/aula4/atividade.html" }
           ]
         },
         // { type: "link", icon: "closing", title: "Encerramento do módulo", path: "/modulo1/encerramento.html" }
@@ -66,7 +66,7 @@ const course = {
             { type: "link", icon: "lesson", title: "Sobre esta aula", path: "/modulo2/aula1/sobre-aula.html" },
             { type: "link", icon: "lesson", title: "Tópico 1: Fundamentos da Toxicologia e Contaminantes", path: "/modulo2/aula1/topico1.html" },
             { type: "link", icon: "lesson", title: "Tópico 2: Casos e Relatos de Sucesso/Fracasso", path: "/modulo2/aula1/topico2.html" },
-            { type: "link", icon: "lesson", title: "Questões de fixação", path: "/modulo2/aula1/atividade.html" }
+            { type: "link", icon: "lesson", title: "Questão de Fixação", path: "/modulo2/aula1/atividade.html" }
           ]
         },
         {
@@ -78,7 +78,7 @@ const course = {
             { type: "link", icon: "lesson", title: "Tópico 2: Conceitos Fundamentais", path: "/modulo2/aula2/topico2.html" },
             { type: "link", icon: "lesson", title: "Tópico 3: Toxicologia e epidemiologia na avaliação de riscos e efeitos à saúde", path: "/modulo2/aula2/topico3.html" },
             { type: "link", icon: "lesson", title: "Tópico 4: Estudo de Casos: Avaliação dos efeitos da exposição a metais tóxicos", path: "/modulo2/aula2/topico4.html" },
-            { type: "link", icon: "lesson", title: "Questões de fixação", path: "/modulo2/aula2/atividade.html" }
+            { type: "link", icon: "lesson", title: "Questão de Fixação", path: "/modulo2/aula2/atividade.html" }
           ]
         },
         {
@@ -93,7 +93,7 @@ const course = {
             { type: "link", icon: "lesson", title: "Tópico 5: Composição química do tabaco e da fumaça do cigarro", path: "/modulo2/aula3/topico5.html" },
             { type: "link", icon: "lesson", title: "Tópico 6: Toxicocinética da nicotina", path: "/modulo2/aula3/topico6.html" },
             { type: "link", icon: "lesson", title: "Tópico 7: Efeitos tóxicos do tabaco", path: "/modulo2/aula3/topico7.html" },
-            { type: "link", icon: "lesson", title: "Questões de fixação", path: "/modulo2/aula3/atividade.html" }
+            { type: "link", icon: "lesson", title: "Questão de Fixação", path: "/modulo2/aula3/atividade.html" }
           ]
         },
         {
@@ -103,7 +103,7 @@ const course = {
             { type: "link", icon: "lesson", title: "Sobre esta aula", path: "/modulo2/aula4/sobre-aula.html" },
             { type: "link", icon: "lesson", title: "Tópico 1: O que é toxicologia dos alimentos?", path: "/modulo2/aula4/topico1.html" },
             { type: "link", icon: "lesson", title: "Tópico 2: Classificação dos contaminantes alimentares", path: "/modulo2/aula4/topico2.html" },
-            { type: "link", icon: "lesson", title: "Questões de fixação", path: "/modulo2/aula4/atividade.html" }
+            { type: "link", icon: "lesson", title: "Questão de Fixação", path: "/modulo2/aula4/atividade.html" }
           ]
         },
         {
@@ -116,7 +116,7 @@ const course = {
             { type: "link", icon: "lesson", title: "Tópico 3: Monitoramento ambiental (MA)", path: "/modulo2/aula5/topico3.html" },
             { type: "link", icon: "lesson", title: "Tópico 4: Monitoramento biológico (MB) ou Biomonitoramento", path: "/modulo2/aula5/topico4.html" },
             { type: "link", icon: "lesson", title: "Tópico 5: Fase clínica: vigilância da saúde e diagnóstico clínico", path: "/modulo2/aula5/topico5.html" },
-            { type: "link", icon: "lesson", title: "Questões de fixação", path: "/modulo2/aula5/atividade.html" }
+            { type: "link", icon: "lesson", title: "Questão de Fixação", path: "/modulo2/aula5/atividade.html" }
           ]
         },
         // { type: "link", icon: "activity", title: "Atividade", path: "/modulo2/atividade.html" },
@@ -135,7 +135,7 @@ const course = {
             { type: "link", icon: "lesson", title: "Sobre esta aula", path: "/modulo3/aula1/sobre-aula.html" },
             { type: "link", icon: "lesson", title: "Tópico 1: Conceitos fundamentais e interações", path: "/modulo3/aula1/topico1.html" },
             { type: "link", icon: "lesson", title: "Tópico 2: Casos clínicos e experiências práticas", path: "/modulo3/aula1/topico2.html" },
-            { type: "link", icon: "lesson", title: "Questões de fixação", path: "/modulo3/aula1/atividade.html" } 
+            { type: "link", icon: "lesson", title: "Questão de Fixação", path: "/modulo3/aula1/atividade.html" } 
           ]
         },
         {
@@ -145,7 +145,7 @@ const course = {
             { type: "link", icon: "lesson", title: "Sobre esta aula", path: "/modulo3/aula2/sobre-aula.html" },
             { type: "link", icon: "lesson", title: "Tópico 1: Aguardando SB", path: "/modulo3/aula2/topico1.html" },
             { type: "link", icon: "lesson", title: "Tópico 2: Aguardando SB", path: "/modulo3/aula2/topico2.html" },
-            { type: "link", icon: "lesson", title: "Questões de fixação", path: "/modulo3/aula2/atividade.html" } 
+            { type: "link", icon: "lesson", title: "Questão de Fixação", path: "/modulo3/aula2/atividade.html" } 
           ]
         },
         {
@@ -155,7 +155,7 @@ const course = {
             { type: "link", icon: "lesson", title: "Sobre esta aula", path: "/modulo3/aula3/sobre-aula.html" },
             { type: "link", icon: "lesson", title: "Tópico 1: Aguardando SB", path: "/modulo3/aula3/topico1.html" },
             { type: "link", icon: "lesson", title: "Tópico 2: Aguardando SB", path: "/modulo3/aula3/topico2.html" },
-            { type: "link", icon: "lesson", title: "Questões de fixação", path: "/modulo3/aula3/atividade.html" } 
+            { type: "link", icon: "lesson", title: "Questão de Fixação", path: "/modulo3/aula3/atividade.html" } 
           ]
         },
         {
@@ -165,7 +165,7 @@ const course = {
             { type: "link", icon: "lesson", title: "Sobre esta aula", path: "/modulo3/aula4/sobre-aula.html" },
             { type: "link", icon: "lesson", title: "Tópico 1: Aguardando SB", path: "/modulo3/aula4/topico1.html" },
             { type: "link", icon: "lesson", title: "Tópico 2: Aguardando SB", path: "/modulo3/aula4/topico2.html" },
-            { type: "link", icon: "lesson", title: "Questões de fixação", path: "/modulo3/aula4/atividade.html" }
+            { type: "link", icon: "lesson", title: "Questão de Fixação", path: "/modulo3/aula4/atividade.html" }
           ]
         },
         {
@@ -175,7 +175,7 @@ const course = {
             { type: "link", icon: "lesson", title: "Sobre esta aula", path: "/modulo3/aula5/sobre-aula.html" },
             { type: "link", icon: "lesson", title: "Tópico 1: Aguardando SB", path: "/modulo3/aula5/topico1.html" },
             { type: "link", icon: "lesson", title: "Tópico 2: Aguardando SB", path: "/modulo3/aula5/topico2.html" },
-            { type: "link", icon: "lesson", title: "Questões de fixação", path: "/modulo3/aula5/atividade.html" }
+            { type: "link", icon: "lesson", title: "Questão de Fixação", path: "/modulo3/aula5/atividade.html" }
           ]
         },
         {
@@ -185,7 +185,7 @@ const course = {
             { type: "link", icon: "lesson", title: "Sobre esta aula", path: "/modulo3/aula6/sobre-aula.html" },
             { type: "link", icon: "lesson", title: "Tópico 1: Aguardando SB", path: "/modulo3/aula6/topico1.html" },
             { type: "link", icon: "lesson", title: "Tópico 2: Aguardando SB", path: "/modulo3/aula6/topico2.html" },
-            { type: "link", icon: "lesson", title: "Questões de fixação", path: "/modulo3/aula6/atividade.html" }
+            { type: "link", icon: "lesson", title: "Questão de Fixação", path: "/modulo3/aula6/atividade.html" }
           ]
         },
         // { type: "link", icon: "activity", title: "Atividade", path: "/modulo3/atividade.html" },
